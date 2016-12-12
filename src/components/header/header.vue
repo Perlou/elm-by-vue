9ca26d925fad3bf8da1df33da2c6a1adcbe4b8da
Perlou <a87387875@163.com>
2016-12-12 23:58:16 +0800

    feat header component

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -37,17 +37,36 @@
 </script>
 
 <style lang="sass">
+  @import '../../common/scss/common/mixin.scss';
+
   .header
     color: #FFFFFF;
     background: #000;
 
-    .header-content-wrap
-      padding: 24px 12px 18px 24px;
+  .header-content-wrap
+    padding: 24px 12px 18px 24px;
+    font-size: 0
 
-      .avatar
-        display: inline-block;
-      .content
-        display: inline-block;
+    .avatar
+      display: inline-block;
 
+    .content
+      display: inline-block;
+      margin-left: 16px;
+      font-size: 14px;
+
+    .title
+      margin: 2px 0 8px 0;
+
+      .brand
+        display: inline-block;
+        width: 30px;
+        height: 18px;
+        vertical-align: top;
+
+      .name
+        margin-left: 6px;
+        font-size: 16px;
+        line-height: 18px;
 
 </style>
